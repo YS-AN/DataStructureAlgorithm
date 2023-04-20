0670bcc756a8cfe5fe605b853f7cac2e6ea00803
YS AN <as5948@naver.com>
2023-04-20 23:04:06 +0900

    feat : 실행 소스 구현

--- StudyDSA/DSA03_Iterator/Program.cs
+++ StudyDSA/DSA03_Iterator/Program.cs
@@ -10,8 +10,14 @@ namespace DSA03_Iterator
 
 			IteratorInLinkedList();
 
+			SortData();
+
+			GetAverage();
 		}
 
+		/// <summary>
+		/// List 반복자
+		/// </summary>
 		static void IteratorInList()
 		{
 			DSA03_Iterator.List<int> list = new DSA03_Iterator.List<int>();
@@ -36,6 +42,9 @@ namespace DSA03_Iterator
 			}
 		}
 
+		/// <summary>
+		/// LinkedList 반복자
+		/// </summary>
 		static void IteratorInLinkedList()
 		{
 			DSA03_Iterator.LinkedList<int> list = new DSA03_Iterator.LinkedList<int>();
@@ -45,6 +54,57 @@ namespace DSA03_Iterator
 			{
 				Console.WriteLine(i);
 			}
+		}
+
+		/// <summary>
+		/// Sort(배열), Sort(리스트) 둘 모두 정렬 가능한 하나의 함수 구현
+		/// </summary>
+		static void SortData()
+		{
+			Sort sort = new Sort();
+
+			System.Collections.Generic.List<int> list = new System.Collections.Generic.List<int>() { 4, 6, 1, 29, 0, 2, 10, 30, 3 };
+			Print(sort.Ascending(list));
+
+			string[] arr = new string[5] { "홍길동", "안예슬", "최영희", "김하늘", "박철수" };
+			Print(sort.Ascending(arr)); Console.WriteLine(Average(list));
+		}
+
+		static void Print<T>(IList<T> data)
+		{
+			foreach (var item in data)
+			{
+				Console.WriteLine(item);
+			}
+			Console.WriteLine();
+		}
+
+		/// <summary>
+		/// 평균 구하기
+		/// </summary>
+		private static void GetAverage()
+		{
+			List<int> list = new List<int>() { 4, 6, 1, 29, 0, 2, 10, 30, 3 };
+			Console.WriteLine($"평균 : {Average(list)}");
+
+			int[] arr = { 4, 6, 1, 29, 0, 2, 10, 30, 3 };
+			Console.WriteLine($"평균 : {Average(arr)}");
+		}
+
+		/// <summary>
+		/// int 자료구조의 평균을 구하는 Average(자료구조) 구현
+		/// </summary>
+		/// <param name="data">데이터</param>
+		/// <returns></returns>
+		static int Average(IEnumerable<int> data)
+		{
+			int sum = 0;
+			foreach(var item in data)
+			{
+				sum += item;
+			}
+
+			return (sum / data.Count());
 		}
 	}
 }
