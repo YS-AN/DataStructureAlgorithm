014369fcd23a57d284398fa19f0eec091e49cd24
YS AN <as5948@naver.com>
2023-04-25 21:46:33 +0900

    feat : Heap 활용 소스 구현

--- StudyDSA/DSA06_Heap/PracticeHeap.cs
+++ StudyDSA/DSA06_Heap/PracticeHeap.cs
@@ -31,21 +31,58 @@ namespace DSA06_Heap
 		/// <summary>
 		/// 우선순위 중간값 구하기
 		/// </summary>
-		public void GetMidData()
+		
+
+		/*
+		 * Heap을 이용한 중간 값 찾기
+		 * 여러 데이터를 정렬 시켜둔 상황에서 중간값을 찾으면 쉽게 찾을 수 있음.
+		 * 하지만 새로운 데이터가 추가 된 경우에는 다시 데이터를 정렬해야 만 중간 값을 찾아야함.
+		 * 추가, 삭제가 있을 때 마다 데이터를 재정렬해줘야 함 (O(nlogn)임
+		 * 
+		 * 중간값이라는것은 왼쪽 n개 오른쪽에 n개 있으면 중간 값이라고 할 수 있음.
+		 * n < 중간값 < n  -> 값 추가 : n < 중간값 < n+2이면, 더이상 중간값이라고 할 수 없음
+		 * 
+		 * 2개 이상 차이면 차이다는 2개 중 
+		 * 큰 값이 2개이상 많으면 가장 작은 값이 중간 값
+		 * 작은 값이 2개이상 차이나면 가장 큰 값이 중간 값이 됨. 
+		 */
+		public int GetMidData(int[] values)
 		{
-			//DSA06_Heap.UserDefinition.PriorityQueue<int, int> minQ = new DSA06_Heap.UserDefinition.PriorityQueue<int, int>();
-			//DSA06_Heap.UserDefinition.PriorityQueue<int, int> maxQ = new DSA06_Heap.UserDefinition.PriorityQueue<int, int>((a,b) => (b-a));
-			//
-			//for (int i = 0; i < 10000;)
-			//{
-			//	priorityQ.Enquere(++i, i);
-			//}
-			//
-			//Node<int, int> midNode = priorityQ.MidNode();
-			//Console.WriteLine($"중간 값 : [{midNode.priority},{midNode.elemenet}]");
+			DSA06_Heap.UserDefinition.PriorityQueue<int, int> minQ = new DSA06_Heap.UserDefinition.PriorityQueue<int, int>(Comparer<int>.Create((a, b) => (b - a)));
+			DSA06_Heap.UserDefinition.PriorityQueue<int, int> maxQ = new DSA06_Heap.UserDefinition.PriorityQueue<int, int>();
+
+			int midVal = values[0];
+
+			for (int i=1; i< values.Length; i++)
+			{
+				if (values[i] > midVal)
+				{
+					maxQ.Enquere(values[i], values[i]);
+				}
+				else
+				{
+					minQ.Enquere(values[i], values[i]);
+				}
+
+				if(Math.Abs(maxQ.Count- minQ.Count) > 1)
+				{
+					if(maxQ.Count > minQ.Count)
+					{
+						minQ.Enquere(midVal, midVal);
+						midVal = maxQ.Dequere();
+					}
+					else
+					{
+						maxQ.Enquere(midVal, midVal);
+						midVal = minQ.Dequere();
+					}
+				}
+
+			}
+
+			return midVal;
+
 		}
 
-
 	}
-	
 }
--- StudyDSA/DSA06_Heap/Program.cs
+++ StudyDSA/DSA06_Heap/Program.cs
@@ -4,12 +4,29 @@
 	{
 		static void Main(string[] args)
 		{
-			//UseHeap	useHeap = new UseHeap();
-			//useHeap.TestPriorityQueue();
+			UseHeap	useHeap = new UseHeap();
+			useHeap.TestPriorityQueue();
+			Console.WriteLine();
 
 			PracticeHeap practiceHeap = new PracticeHeap();
 			practiceHeap.SortEmergency();
-			practiceHeap.GetMidData();
+			Console.WriteLine();
+
+			int[] values01 = new int[10000];
+			for (int i = 0; i < 10000; ) { values01[i] = ++i; }
+			Console.WriteLine(practiceHeap.GetMidData(values01));
+			Console.WriteLine();
+
+			Random random = new Random(Guid.NewGuid().GetHashCode());
+
+			int[] values02 = new int[10];
+			for (int i = 0; i < 10; i++) { values02[i] = random.Next(1, 500); }
+			
+			//랜덤 데이터 확인
+			foreach(var item in values02) { Console.WriteLine(item); }
+			Console.WriteLine();
+
+			Console.WriteLine(practiceHeap.GetMidData(values02)); //중간값 출력
 		}
 	}
 }
