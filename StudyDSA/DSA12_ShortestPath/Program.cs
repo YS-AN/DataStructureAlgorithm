5e9e7b8ed0d48f790107c00455fbd350b1980c48
YS AN <as5948@naver.com>
2023-05-02 21:35:32 +0900

    feat : 최단경로 탐색 알고리즘

---
+++ StudyDSA/DSA12_ShortestPath/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace DSA12_ShortestPath
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			const int INF = 99999;
+
+			int[,] graph = new int[9, 9]
+			{
+				{   0, INF,   1,   7, INF, INF, INF,   5, INF},
+				{ INF,   0, INF, INF, INF,   4, INF, INF, INF},
+				{ INF, INF,   0, INF, INF, INF, INF, INF, INF},
+				{   5, INF, INF,   0, INF, INF, INF, INF, INF},
+				{ INF, INF,   9, INF,   0, INF, INF, INF,   2},
+				{   1, INF, INF, INF, INF,   0, INF,   6, INF},
+				{ INF, INF, INF, INF, INF, INF,   0, INF, INF},
+				{   1, INF, INF, INF,   4, INF, INF,   0, INF},
+				{ INF,   5, INF,   2, INF, INF, INF, INF,   0}
+			};
+
+
+			int[] distance;
+			int[] path;
+
+			Dijkstra dijkstra = new Dijkstra();
+			dijkstra.ShortestPath(in graph, 0, out distance, out path);
+			Console.WriteLine("<Dijkstra>");
+			dijkstra.PrintDijkstra(distance, path);
+
+            Console.WriteLine("\n===========================================================\n");
+			
+            int[,] costTable;
+			int[,] pathTable;
+			FloydWarshall floydWarshall = new FloydWarshall();
+			floydWarshall.ShortestPath(in graph, out costTable, out pathTable);
+			Console.WriteLine("<Floyd-Warshall>");
+			floydWarshall.PrintFloydWarshall(costTable, pathTable);
+		}
+	}
+
+}
